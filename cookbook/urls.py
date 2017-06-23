55edc1d726382260c6bfa1ed323e64b40f1caa89
Julien SMAGUE <julien.smague@laposte.net>
2017-06-23 23:19:36 +0200

    Ajout du lien vers l'ajout de recette

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from cookbook import views
-from cookbook.views import afficher
+from cookbook.views import afficher, connexion, ajouter
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
     url(r'^afficher/$', afficher, name='afficher'),
-    url(r'^connexion$', views.connexion, name='connexion'),
+    url(r'^connexion$', connexion, name='connexion'),
+    url(r'^ajouter/$', ajouter, name='ajouter'),
 ]
