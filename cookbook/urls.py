a3290421c071c3803297293f9175194287c9ad7c
Julien SMAGUE <julien.smague@laposte.net>
2017-06-24 13:54:51 +0200

    Ajout du lien d'inscription

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -2,12 +2,12 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from cookbook import views
-from cookbook.views import afficher, connexion, ajouter
+from cookbook.views import afficher, ajouter, inscription
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
     url(r'^afficher/$', afficher, name='afficher'),
-    url(r'^connexion$', connexion, name='connexion'),
     url(r'^ajouter/$', ajouter, name='ajouter'),
+    url(r'^inscription/$', inscription, name='inscription'),
 ]
