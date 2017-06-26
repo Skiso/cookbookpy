3a4df42c9f8aace4a3f67f3db1487bcd66d9cdbb
Julien SMAGUE <julien.smague@laposte.net>
2017-06-26 14:02:18 +0200

    Modification de admin lié au changement de model

--- cookbook/admin.py
+++ cookbook/admin.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
-from cookbook.models import Recette, Commentaire, Etapes_Recette, Liste_Ingredients, Ingredients, Note
+from cookbook.models import Recette, Commentaire, Note
 
 admin.site.register(Recette)
 admin.site.register(Commentaire)
-admin.site.register(Etapes_Recette)
-admin.site.register(Liste_Ingredients)
-admin.site.register(Ingredients)
 admin.site.register(Note)
 
 
