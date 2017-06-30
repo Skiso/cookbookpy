95b2cb510f61dfede62e6362274dab5c3b90a116
Julien SMAGUE <julien.smague@laposte.net>
2017-06-30 12:23:46 +0200

    premier test insertion recette

--- cookbook/tests.py
+++ cookbook/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from cookbook.models import Recette
 
-# Create your tests here.
+class RecetteTest(TestCase):
+
+    def test_recette_existe(self):
+        pomme = Recette.objects.get(titre="Pommes sur île flottante")
+        pomme.save()
+        self.assertEqual(pomme.titre, "Pommes sur île flottante")
+        self.assertEqual(pomme.type, "D")
+        self.assertEqual(pomme.difficulte, 1)
+        self.assertEqual(pomme.cout, 10)
+        self.assertEqual(pomme.temps_prepa, 10)
+        self.assertEqual(pomme.temps_cuisson, 10)
+        self.assertEqual(pomme.temps_repos, 0)
+
+
+
+
